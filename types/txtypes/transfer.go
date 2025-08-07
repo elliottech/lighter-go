4981d52b3a9f0e0a0044a6ddac39ebd9a502f92d
Bogdan Pop <pbppbp17@gmail.com>
2025-08-07 14:33:38 +0300

    update hash

--- types/txtypes/transfer.go
+++ types/txtypes/transfer.go
@@ -82,7 +82,7 @@ func (txInfo *L2TransferTxInfo) GetTxInfo() (string, error) {
 }
 
 func (txInfo *L2TransferTxInfo) Hash(lighterChainId uint32, extra ...g.Element) (msgHash []byte, err error) {
-	elems := make([]g.Element, 0, 9)
+	elems := make([]g.Element, 0, 11)
 
 	elems = append(elems, g.FromUint32(lighterChainId))
 	elems = append(elems, g.FromUint32(TxTypeL2Transfer))
@@ -93,6 +93,9 @@ func (txInfo *L2TransferTxInfo) Hash(lighterChainId uint32, extra ...g.Element) 
 	elems = append(elems, g.FromUint32(uint32(txInfo.ApiKeyIndex)))
 	elems = append(elems, g.FromInt64(txInfo.ToAccountIndex))
 	elems = append(elems, g.FromUint64(uint64(txInfo.USDCAmount)&0xFFFFFFFF)) //nolint:gosec
+	elems = append(elems, g.FromUint64(uint64(txInfo.USDCAmount)>>32))        //nolint:gosec
+	elems = append(elems, g.FromUint64(uint64(txInfo.Fee)&0xFFFFFFFF))        //nolint:gosec
+	elems = append(elems, g.FromUint64(uint64(txInfo.Fee)>>32))               //nolint:gosec
 	elems = append(elems, g.FromUint64(uint64(txInfo.USDCAmount)>>32))        //nolint:gosec
 
 	return p2.HashToQuinticExtension(elems).ToLittleEndianBytes(), nil
