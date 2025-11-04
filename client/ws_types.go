ef866cd1e7adf0c3ef7aa4cb7ffc53dff907f0a4
xujun <xujun@taou.com>
2025-11-04 16:17:47 +0800

    add taker_fee in WSTrade

--- client/ws_types.go
+++ client/ws_types.go
@@ -33,44 +33,44 @@ type WSOrderBookUpdate struct {
 	Timestamp int64      `json:"timestamp"`
 }
 
-// Note: WSTickerUpdate and WSTradeUpdate types removed because 
+// Note: WSTickerUpdate and WSTradeUpdate types removed because
 // ticker and trade streams are not supported by Lighter WebSocket API
 
 // Account data types
 type WSAccountUpdate struct {
-	Account              int64                       `json:"account"`
-	Channel              string                      `json:"channel"`
-	Type                 string                      `json:"type"`
-	DailyTradesCount     int                         `json:"daily_trades_count"`
-	DailyVolume          float64                     `json:"daily_volume"`
-	MonthlyTradesCount   int                         `json:"monthly_trades_count"`
-	MonthlyVolume        float64                     `json:"monthly_volume"`
-	TotalTradesCount     int                         `json:"total_trades_count"`
-	TotalVolume          float64                     `json:"total_volume"`
-	WeeklyTradesCount    int                         `json:"weekly_trades_count"`
-	WeeklyVolume         float64                     `json:"weekly_volume"`
-	Positions            map[string]*WSPosition      `json:"positions"`
-	Shares               []WSShare                   `json:"shares"`
-	Trades               map[string][]WSTrade        `json:"trades"`
-	FundingHistories     map[string]interface{}      `json:"funding_histories"`
+	Account            int64                  `json:"account"`
+	Channel            string                 `json:"channel"`
+	Type               string                 `json:"type"`
+	DailyTradesCount   int                    `json:"daily_trades_count"`
+	DailyVolume        float64                `json:"daily_volume"`
+	MonthlyTradesCount int                    `json:"monthly_trades_count"`
+	MonthlyVolume      float64                `json:"monthly_volume"`
+	TotalTradesCount   int                    `json:"total_trades_count"`
+	TotalVolume        float64                `json:"total_volume"`
+	WeeklyTradesCount  int                    `json:"weekly_trades_count"`
+	WeeklyVolume       float64                `json:"weekly_volume"`
+	Positions          map[string]*WSPosition `json:"positions"`
+	Shares             []WSShare              `json:"shares"`
+	Trades             map[string][]WSTrade   `json:"trades"`
+	FundingHistories   map[string]interface{} `json:"funding_histories"`
 }
 
 type WSPosition struct {
-	MarketId                 uint8   `json:"market_id"`
-	Symbol                   string  `json:"symbol"`
-	InitialMarginFraction    string  `json:"initial_margin_fraction"`
-	OpenOrderCount           int     `json:"open_order_count"`
-	PendingOrderCount        int     `json:"pending_order_count"`
-	PositionTiedOrderCount   int     `json:"position_tied_order_count"`
-	Sign                     int8    `json:"sign"`
-	Position                 string  `json:"position"`
-	AvgEntryPrice            string  `json:"avg_entry_price"`
-	PositionValue            string  `json:"position_value"`
-	UnrealizedPnl            string  `json:"unrealized_pnl"`
-	RealizedPnl              string  `json:"realized_pnl"`
-	LiquidationPrice         string  `json:"liquidation_price"`
-	MarginMode               int     `json:"margin_mode"`
-	AllocatedMargin          string  `json:"allocated_margin"`
+	MarketId               uint8  `json:"market_id"`
+	Symbol                 string `json:"symbol"`
+	InitialMarginFraction  string `json:"initial_margin_fraction"`
+	OpenOrderCount         int    `json:"open_order_count"`
+	PendingOrderCount      int    `json:"pending_order_count"`
+	PositionTiedOrderCount int    `json:"position_tied_order_count"`
+	Sign                   int8   `json:"sign"`
+	Position               string `json:"position"`
+	AvgEntryPrice          string `json:"avg_entry_price"`
+	PositionValue          string `json:"position_value"`
+	UnrealizedPnl          string `json:"unrealized_pnl"`
+	RealizedPnl            string `json:"realized_pnl"`
+	LiquidationPrice       string `json:"liquidation_price"`
+	MarginMode             int    `json:"margin_mode"`
+	AllocatedMargin        string `json:"allocated_margin"`
 }
 
 type WSShare struct {
@@ -80,27 +80,28 @@ type WSShare struct {
 }
 
 type WSTrade struct {
-	TradeId                               int64  `json:"trade_id"`
-	TxHash                                string `json:"tx_hash"`
-	Type                                  string `json:"type"`
-	MarketId                              int    `json:"market_id"`
-	Size                                  string `json:"size"`
-	Price                                 string `json:"price"`
-	UsdAmount                             string `json:"usd_amount"`
-	AskId                                 int64  `json:"ask_id"`
-	BidId                                 int64  `json:"bid_id"`
-	AskAccountId                          int64  `json:"ask_account_id"`
-	BidAccountId                          int64  `json:"bid_account_id"`
-	IsMakerAsk                            bool   `json:"is_maker_ask"`
-	BlockHeight                           int64  `json:"block_height"`
-	Timestamp                             int64  `json:"timestamp"`
-	TakerPositionSizeBefore              string `json:"taker_position_size_before"`
-	TakerEntryQuoteBefore                string `json:"taker_entry_quote_before"`
-	TakerInitialMarginFractionBefore     int    `json:"taker_initial_margin_fraction_before"`
-	MakerFee                             int    `json:"maker_fee"`
-	MakerPositionSizeBefore              string `json:"maker_position_size_before"`
-	MakerEntryQuoteBefore                string `json:"maker_entry_quote_before"`
-	MakerInitialMarginFractionBefore     int    `json:"maker_initial_margin_fraction_before"`
+	TradeId                          int64  `json:"trade_id"`
+	TxHash                           string `json:"tx_hash"`
+	Type                             string `json:"type"`
+	MarketId                         int    `json:"market_id"`
+	Size                             string `json:"size"`
+	Price                            string `json:"price"`
+	UsdAmount                        string `json:"usd_amount"`
+	AskId                            int64  `json:"ask_id"`
+	BidId                            int64  `json:"bid_id"`
+	AskAccountId                     int64  `json:"ask_account_id"`
+	BidAccountId                     int64  `json:"bid_account_id"`
+	IsMakerAsk                       bool   `json:"is_maker_ask"`
+	BlockHeight                      int64  `json:"block_height"`
+	Timestamp                        int64  `json:"timestamp"`
+	TakerFee                         int    `json:"taker_fee"`
+	TakerPositionSizeBefore          string `json:"taker_position_size_before"`
+	TakerEntryQuoteBefore            string `json:"taker_entry_quote_before"`
+	TakerInitialMarginFractionBefore int    `json:"taker_initial_margin_fraction_before"`
+	MakerFee                         int    `json:"maker_fee"`
+	MakerPositionSizeBefore          string `json:"maker_position_size_before"`
+	MakerEntryQuoteBefore            string `json:"maker_entry_quote_before"`
+	MakerInitialMarginFractionBefore int    `json:"maker_initial_margin_fraction_before"`
 }
 
 type WSOrderUpdate struct {
@@ -124,7 +125,7 @@ const (
 	ChannelOrders    = "orders"
 	// The following channels are not supported by Lighter WebSocket API:
 	// ChannelTicker    = "ticker"      // REMOVED - not supported
-	// ChannelTrades    = "trades"      // REMOVED - not supported  
+	// ChannelTrades    = "trades"      // REMOVED - not supported
 	// ChannelMarkPrice = "markprice"   // REMOVED - not supported
 )
 
@@ -137,22 +138,22 @@ const (
 	MessageTypeConnected    = "connected"
 	MessageTypeSubscribed   = "subscribed"
 	MessageTypeUnsubscribed = "unsubscribed"
-	
+
 	// Subscription confirmation messages
 	MessageTypeOrderBookSubscribed = "subscribed/order_book"
 	MessageTypeAccountSubscribed   = "subscribed/account_all"
-	
+
 	// Data update messages (the actual data streams)
 	MessageTypeOrderBookUpdate = "update/order_book"
 	MessageTypeAccountUpdate   = "update/account_all"
-	
+
 	// Deprecated: Use MessageTypeOrderBookUpdate instead
 	MessageTypeOrderBook = "update/order_book"
-	// Deprecated: Use MessageTypeAccountUpdate instead  
-	MessageTypeAccount   = "update/account_all"
+	// Deprecated: Use MessageTypeAccountUpdate instead
+	MessageTypeAccount = "update/account_all"
 )
 
-// WSPriceLevel represents a single price level in WebSocket orderbook messages  
+// WSPriceLevel represents a single price level in WebSocket orderbook messages
 type WSPriceLevel struct {
 	Price string `json:"price"`
 	Size  string `json:"size"`
